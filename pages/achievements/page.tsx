becf28ad2fa5e9c17f3e935bc000a9a76c989e7c
Dilkash Maryam <160109487+DilkashMaryam@users.noreply.github.com>
2024-11-27 14:52:05 +0500

    Update page.tsx

--- pages/achievements/page.tsx
+++ pages/achievements/page.tsx
@@ -3,14 +3,14 @@ import React from 'react'
 
 const Achieve = () => {
   return (
-    <div className='w-full h-[488px] bg-[#FFFFFF] sm:max-md:w-[428px] sm:max-md:h-auto sm:max-md:mt-[100px]'>
+    <div className='w-full h-[488px] bg-[#FFFFFF] sm:max-md:w-full sm:max-md:h-auto sm:max-md:mt-[100px]'>
       <div className='w-[1152px] h-[136px] gap-[16px] text-center sm:max-md:w-full sm:max-md:h-auto'>
-        <h1 className='font-Roboto font-bold text-[48px] leading-[57.6px] pl-[100px] sm:max-md:h-[24px] sm:max-md:text-[36px] sm:max-md:pl-10'>Our Achivements</h1>
+        <h1 className='font-Roboto font-bold text-[48px] leading-[57.6px] pl-[100px] sm:max-md:h-[24px] sm:max-md:text-[48px] sm:max-md:pl-10'>Our Achivements</h1>
         <p className='font-Roboto text-[18px] font-normal leading-[27px] pl-[100px] pt-4 sm:max-md:hidden'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.</p>
-        <p className='sm:max-md:h-[81px] sm:max-md:text-[20px] sm:max-md:pt-11 sm:max-md:font-Roboto sm:max-md:font-normal sm:max-md:pl-10 lg:hidden'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. </p>
+        <p className='sm:max-md:h-[81px] sm:max-md:text-[20px] sm:max-md:pt-11 sm:max-md:font-Roboto sm:max-md:font-normal sm:max-md:pl-10 xl:hidden'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. </p>
       </div>
       <div className='w-full h-[96px] gap-[200px] mt-10 sm:max-md:grid sm:max-md:grid-cols-2 flex sm:max-md:mt-[100px] sm:max-md:w-full sm:max-md:h-[176px] sm:max-md:gap-24'>
-        <div className='w-[91px] h-[48px] ml-[150px] sm:max-md:w-[128px] sm:max-md:h-[60px] sm:max-md:gap-8 sm:max-md:ml-24'>
+        <div className='w-[91px] h-[48px] ml-[150px] sm:max-md:w-[128px] sm:max-md:h-[60px] sm:max-md:gap-8 sm:max-md:ml-32'>
             <h1 className='font-Roboto font-bold text-[40px] leading-[48px] sm:max-md:w-[46px] sm:max-md:h-[28px] sm:max-md:text-[24px]'>+200</h1>
             <p className='font-Roboto font-normal text-[16px] leading-[24px] sm:max-md:w-[59px] sm:max-md:h-[24px] sm:max-md:text-[16px] sm:max max-md:mt-4'>Courses</p>
         </div>
@@ -18,7 +18,7 @@ const Achieve = () => {
             <h1 className='font-Roboto font-bold text-[40px] leading-[48px] sm:max-md:w-[46px] sm:max-md:h-[28px] sm:max-md:text-[24px]'>50K</h1>
             <p className='font-Roboto font-normal text-[16px] leading-[24px] sm:max-md:w-[59px] sm:max-md:h-[24px] sm:max-md:text-[16px] sm:max max-md:mt-4'>Mentors</p>
         </div>
-        <div className='w-[91px] h-[48px] ml-[50px] sm:max-md:w-[128px] sm:max-md:h-[60px] sm:max-md:gap-8 sm:max-md:ml-24'>
+        <div className='w-[91px] h-[48px] ml-[50px] sm:max-md:w-[128px] sm:max-md:h-[60px] sm:max-md:gap-8 sm:max-md:ml-32'>
             <h1 className='font-Roboto font-bold text-[40px] leading-[48px] sm:max-md:w-[46px] sm:max-md:h-[28px] sm:max-md:text-[24px]'>370K</h1>
             <p className='font-Roboto font-normal text-[16px] leading-[24px] sm:max-md:w-[59px] sm:max-md:h-[24px] sm:max-md:text-[16px] sm:max max-md:mt-4'>Students</p>
         </div>

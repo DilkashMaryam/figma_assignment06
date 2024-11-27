cbe17e686aea6e8c638b72c0c41b7179eaff6cdd
Dilkash Maryam <160109487+DilkashMaryam@users.noreply.github.com>
2024-11-27 12:55:17 +0500

    Update Footer.tsx

--- pages/components/Footer/Footer.tsx
+++ pages/components/Footer/Footer.tsx
@@ -8,30 +8,30 @@ import Image from 'next/image';
 
 const Footer = () => {
   return (
-    <div className='w-full h-[800px] mt-[100px] gap-[80px] bg-[#FFFFFF] max-md:w-full max-md:h-[1411px] max-md:mt-[100px] max-md:flex-col'>
-      <div className='w-[1120px] h-[82px] justify-between flex max-md:w-full max-md:h-[91px] max-md:gap-16 max-md:flex-col'>
-        <div className='w-[500px] h-[51px] ml-28 mt-40 max-md:w-full max-md:h-[91px] max-md:gap-16'>
-            <h1 className='w-[500px] h-[27px] font-Roboto font-semibold text-[18px] max-md:w-full max-md:h-[27px] max-md:text-[24px] max-md:pr-16'>Subscribe to our newsletter</h1>
-            <p className='w-[500px] h-[24px] font-Roboto font-normal text-[16px] max-md:w-full max-md:h-[48px] max-md:text-center max-md:pr-40 max-md:text-[26px] max-md:pt-8'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    <div className='w-full h-[800px] mt-[100px] gap-[80px] bg-[#FFFFFF] sm:max-md:w-[428px] sm:max-md:h-[1411px] sm:max-md:mt-[100px] sm:max-md:flex-col'>
+      <div className='w-[1120px] h-[82px] justify-between flex sm:max-md:w-full sm:max-md:h-[91px] sm:max-md:gap-16 sm:max-md:flex-col'>
+        <div className='w-[500px] h-[51px] ml-28 mt-40 sm:max-md:w-full sm:max-md:h-[91px] sm:max-md:gap-16 sm:max-md:ml-9'>
+            <h1 className='w-[500px] h-[27px] font-Roboto font-semibold text-[18px] sm:max-md:w-full sm:max-md:h-[27px] sm:max-md:text-[24px]'>Subscribe to our newsletter</h1>
+            <p className='w-[500px] h-[24px] font-Roboto font-normal text-[16px] sm:max-md:w-full sm:max-md:h-[48px] sm:max-md:text-center sm:max-md:pr-40 sm:max-md:text-[20px] sm:max-md:pt-8'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </div>
-        <div className='w-[400px] h-[82px] mt-[150px] ml-[250px] max-md:w-full max-md:h-[146px] max-md:gap-16 max-md:ml-5'>
-            <div className='w-[400px] h-[48px] max-md:w-full max-md:h-[112px]'>
+        <div className='w-[400px] h-[82px] mt-[150px] ml-[250px] sm:max-md:w-[428px] sm:max-md:h-[146px] sm:max-md:gap-16 sm:max-md:ml-5'>
+            <div className='w-[400px] h-[48px] sm:max-md:w-full sm:max-md:h-[112px]'>
                 <input type="email" name="email" id="email" placeholder='Enter your email' 
-                className='w-[265px] h-[48px] rounded border-[1px] border-[#000000] p-5 gap-8 max-md:w-full max-md:h-[48px]'/>
-                <button className='w-[119px] h-[48px] ml-[10px] rounded border-[1px] font-Roboto font-normal text-[16px] text-[#000000] border-[#000000] max-md:w-full max-md:h-[48px] max-md:ml-0 max-md:mt-3'>Subscribe</button>
-                <p className='w-[400px] h-[18px] font-Roboto font-normal text-[12px] mt-2 max-md:w-full max-md:h-[18px] max-md:text-[12px] max-md:ml-20'>By subscribing you agree to with our 
-                <a href="#" className='w-[400px] h-[18px] font-Roboto font-normal text-[12px] mt-2 max-md:w-full max-md:h-[18px] max-md:text-[12px] underline'> Privacy Policy</a></p>
+                className='w-[265px] h-[48px] rounded border-[1px] border-[#000000] p-5 gap-8 sm:max-md:w-full sm:max-md:h-[48px]'/>
+                <button className='w-[119px] h-[48px] ml-[10px] rounded border-[1px] font-Roboto font-normal text-[16px] text-[#000000] border-[#000000] sm:max-md:w-full sm:max-md:h-[48px] sm:max-md:ml-0 sm:max-md:mt-3'>Subscribe</button>
+                <p className='w-[400px] h-[18px] font-Roboto font-normal text-[12px] mt-2 sm:max-md:w-full sm:max-md:h-[18px] sm:max-md:text-[12px] sm:max-md:ml-20'>By subscribing you agree to with our 
+                <a href="#" className='w-[400px] h-[18px] font-Roboto font-normal text-[12px] mt-2 sm:ax-md:w-full sm:max-md:h-[18px] sm:max-md:text-[12px] underline'> Privacy Policy</a></p>
             </div>
         </div>
       </div>
-      <div className='w-[1120px] h-[225px] flex gap-[40px] mt-[300px] ml-32 max-md:mt-[600px] max-md:flex-col max-md:w-full max-md:h-[811px]'>
-          <div className='w-[250px] h-[40px] flex'>
+      <div className='w-[1120px] h-[225px] flex gap-[40px] mt-[300px] ml-32 sm:max-md:mt-[600px] sm:max-md:flex-col sm:max-md:w-full sm:max-md:h-[811px]'>
+          <div className='w-[250px] h-[40px] flex sm:max-md:ml-8'>
             <Image src="/images/Frame 1.png" alt='logo' width={32.58} height={30.38}></Image>
             <h1 className='w-[90px] h-[30px] fontRoboto ml-4 font-bold text-[25.07px]'>Ddsgnr</h1>
           </div>
-          <div className='w-[250px] h-[225px] max-md:ml-10'>
-            <h1 className='w-[250px] h-[24px] font-Roboto font-semibold text-[16px]'>Courses</h1>
-            <ul className='w-[250px] h-[185px] pt-0'>
+          <div className='w-[250px] h-[225px]'>
+            <h1 className='w-[250px] h-[24px] font-Roboto font-semibold text-[16px] sm:max-md:text-center text-center'>Courses</h1>
+            <ul className='w-[250px] h-[185px] pt-0 sm:max-md:text-center text-center'>
                 <li className='font-Roboto font-normal text-[14px] w-[250px] h-[21px] pt-8'>Business</li>
                 <li className='font-Roboto font-normal text-[14px] w-[250px] h-[21px] pt-8'>Development</li>
                 <li className='font-Roboto font-normal text-[14px] w-[250px] h-[21px] pt-8 '>Technology</li>
@@ -39,9 +39,9 @@ const Footer = () => {
                 <li className='font-Roboto font-normal text-[14px] w-[250px] h-[21px] pt-8'>Programming</li>
             </ul>
           </div>
-          <div className='w-[250px] h-[225px] gap-[16px] max-md:ml-10'>
-            <h1 className='w-[250px] h-[24px] font-Roboto font-semibold text-[16px]'>Resources</h1>
-            <ul className='w-[250px] h-[185px] pt-0'>
+          <div className='w-[250px] h-[225px] gap-[16px]'>
+            <h1 className='w-[250px] h-[24px] font-Roboto font-semibold text-[16px] sm:max-md:text-center text-center'>Resources</h1>
+            <ul className='w-[250px] h-[185px] pt-0 sm:max-md:text-center text-center'>
                 <li className='font-Roboto font-normal text-[14px] w-[250px] h-[21px] pt-8'>Career</li>
                 <li className='font-Roboto font-normal text-[14px] w-[250px] h-[21px] pt-8'>Resume</li>
                 <li className='font-Roboto font-normal text-[14px] w-[250px] h-[21px] pt-8 '>Learning</li>
@@ -49,9 +49,9 @@ const Footer = () => {
                 <li className='font-Roboto font-normal text-[14px] w-[250px] h-[21px] pt-8'>Jobs</li>
             </ul>
           </div>
-          <div className='w-[250px] h-[225px] gap-[16px] max-md:ml-10'>
-            <h1 className='w-[250px] h-[24px] font-[Roboto] font-semibold text-[16px]'>About Us</h1>
-            <ul className='w-[250px] h-[185px] pt-0'>
+          <div className='w-[250px] h-[225px] gap-[16px]'>
+            <h1 className='w-[250px] h-[24px] font-Roboto font-semibold text-[16px] sm:max-md:text-center text-center'>About Us</h1>
+            <ul className='w-[250px] h-[185px] pt-0 sm:max-md:text-center text-center'>
                 <li className='font-Roboto font-normal text-[14px] w-[250px] h-[21px] pt-8'>Contact</li>
                 <li className='font-Roboto font-normal text-[14px] w-[250px] h-[21px] pt-8'>Help/Support</li>
                 <li className='font-Roboto font-normal text-[14px] w-[250px] h-[21px] pt-8 '>FAQ</li>
@@ -60,18 +60,18 @@ const Footer = () => {
             </ul>
           </div>
       </div>
-      <div className="w-full bg-[#FFFFFF] mt-[100px] max-md:mt-[20px] max-md:w-full max-md:h-[147px] max-md:gap-32">
-  <hr className="w-[1120px] h-1 bg-[#000000] mt-20 ml-28 max-md:ml-3 max-md:w-full"/>
-  <div className="w-full flex justify-between items-center mt-8 px-28 max-md:flex-col max-md:w-full max-md:h-[114px]">
-    <div className='flex gap-4 items-center max-md:flex-col max-md:gap-6'>
-      <span className="font-Roboto font-normal text-[14px] max-md:w-[195px] max-md:h-[21px] max-md:text-[12px] max-md:ml-10">2023 Ddsgnr. All rights reserved.</span>
-     <div className=' max-md:w-[345px] max-md:h-[21px] max-md:text-center'>
+      <div className="w-full bg-[#FFFFFF] mt-[100px] sm:max-md:mt-[20px] sm:max-md:w-full m:max-md:h-[147px] sm:max-md:gap-32">
+  <hr className="w-[1120px] h-1 bg-[#000000] mt-20 ml-28 sm:max-md:ml-3 sm:max-md:w-full"/>
+  <div className="w-full flex justify-between items-center mt-8 px-28 sm:max-md:flex-col sm:max-md:w-full sm:max-md:h-[114px]">
+    <div className='flex gap-4 items-center sm:max-md:flex-col sm:max-md:gap-6'>
+      <span className="font-Roboto font-normal text-[14px] sm:max-md:w-[195px] sm:max-md:h-[21px] sm:max-md:text-[12px] sm:max-md:ml-10">2023 Ddsgnr. All rights reserved.</span>
+     <div className=' max-md:w-[345px] sm:max-md:h-[21px] sm:max-md:text-center'>
      <span className="font-Roboto font-normal text-[14px] underline">Privacy Policy</span>
-      <span className="font-Roboto font-normal text-[14px] underline max-md:ml-5 lg:ml-4">Terms of Service</span>
-      <span className="font-Roboto font-normal text-[14px] underline max-md:ml-5 lg:ml-4">Cookies Settings</span>
+      <span className="font-Roboto font-normal text-[14px] underline sm:max-md:ml-5 lg:ml-4">Terms of Service</span>
+      <span className="font-Roboto font-normal text-[14px] underline sm:max-md:ml-5 lg:ml-4">Cookies Settings</span>
       </div>
     </div>
-    <div className="flex gap-4 items-center max-md:mt-5">
+    <div className="flex gap-4 items-center sm:max-md:mt-5">
       <FaFacebookF />
       <FaInstagram />
       <FaTwitter />
